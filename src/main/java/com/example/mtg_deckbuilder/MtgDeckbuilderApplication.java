7ea4f332a11dcdc272ca5be1c396e3eb8da44f27
David <Daviddeltoro1999@gmail.com>
2026-01-31 22:27:13 -0800

    intial commit

---
+++ src/main/java/com/example/mtg_deckbuilder/MtgDeckbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtg_deckbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgDeckbuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgDeckbuilderApplication.class, args);
+    }
+
+}
