ee18c9f47c59febbcafaa7d3c6d745e4bda691c2
David <Daviddeltoro1999@gmail.com>
2026-02-11 19:38:33 -0800

    Add dashboard controller

---
+++ src/main/java/com/example/mtg_deckbuilder/controller/DashboardController.java
@@ -0,0 +1,8 @@
+package com.example.mtg_deckbuilder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DashboardController {
+}
