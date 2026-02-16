811938051a2c00483726dbdee67a225ee496552b
David <Daviddeltoro1999@gmail.com>
2026-02-15 19:28:42 -0800

    Add UserRepository with methods for user retrieval and registration

---
+++ src/main/java/com/example/mtg_deckbuilder/repository/UserRepository.java
@@ -0,0 +1,39 @@
+package com.example.mtg_deckbuilder.repository;
+
+import com.example.mtg_deckbuilder.dto.UserRegistrationDto;
+import com.example.mtg_deckbuilder.model.Card;
+import com.example.mtg_deckbuilder.model.User;
+import com.example.mtg_deckbuilder.utils.Inequality;
+import com.example.mtg_deckbuilder.utils.InequalityParser;
+import com.example.mtg_deckbuilder.utils.Parser;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class UserRepository {
+    private final JdbcClient jdbcClient;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserRepository(JdbcClient jdbcClient, PasswordEncoder passwordEncoder) {
+        this.jdbcClient = jdbcClient;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public Optional<User> findByUsername(String username) {
+        return jdbcClient.sql("SELECT * FROM \"users\" WHERE username = :username")
+                .param("username", username)
+                .query(User.class)
+                .optional();
+    }
+    public void saveUser(UserRegistrationDto dto) {
+    jdbcClient.sql(" INSERT INTO users (id, username, email, password) VALUES (:id, :username, :email, :password)")
+        .param("id", UUID.randomUUID()) // Generates the UUID automatically
+        .param("username", dto.getUsername())
+        .param("email", dto.getEmail())
+        .param("password", passwordEncoder.encode(dto.getPassword())) // Hashes the password
+        .update(); // Use .update() for INSERT/UPDATE/DELETE
+    }
+}
