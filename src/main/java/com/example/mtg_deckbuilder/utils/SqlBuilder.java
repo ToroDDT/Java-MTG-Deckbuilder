ede730aaca769ff6ac812d38acddd286faf01e14
David <Daviddeltoro1999@gmail.com>
2026-02-25 09:56:02 -0800

    Add builder methods for dynamic SQL condition generation in SqlBuilder

--- src/main/java/com/example/mtg_deckbuilder/utils/SqlBuilder.java
+++ src/main/java/com/example/mtg_deckbuilder/utils/SqlBuilder.java
@@ -1,27 +1,75 @@
 package com.example.mtg_deckbuilder.utils;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SqlBuilder {
-    private final String sql;
 
-    public static class Builder {
-        private final String sql;
+  private final String sql;
 
-        public Builder(String sql) {
-            this.sql = sql;
-        }
-        public Builder whereCMC(StringBuilder sql, Map<String, Object> params, String cmcInput) {
-            if (!cmcInput.isBlank()) {
-                sql.append(" AND cmc = :cmc");
-                params.put("cmc", cmcInput);
-            }
-        }
+  public static class Builder {
 
-        public SqlBuilder build() {
-        }
+    private String sql;
+
+    List<String> sqlConditions = new ArrayList<>();
+
+    public Builder(String sql) {
+      this.sql = sql;
     }
 
-    public SqlBuilder(Builder builder) {
+    public Builder whereCMC(Boolean includeCMC, String cmcInequality) {
+      if (includeCMC) {
+        Inequality expression = SearchParametersParser.parseInequality(cmcInequality);
+        String sql = "AND cmc " + expression.operator() + " :cmc";
+        sqlConditions.add(sql);
+        return this;
+      }
+      return this;
     }
+
+    public Builder whereName(Boolean includeName, String name) {
+      if (includeName) {
+        String sql = "AND name ILIKE CONCAT('%', :name, '%')";
+        sqlConditions.add(sql);
+        return this;
+      }
+      return this;
+    }
+
+    public Builder wherePower(Boolean includePower, String powerInequality) {
+      if (includePower) {
+        Inequality expression = SearchParametersParser.parseInequality(powerInequality);
+        String sql = "AND power " + expression.operator() + " :power";
+        sqlConditions.add(sql);
+        return this;
+      }
+      return this;
+    }
+
+    public Builder whereType(String type) {
+      if (!type.isBlank()) {
+        sqlConditions.add("AND type = :type");
+        return this;
+      }
+      return this;
+    }
+
+    public SqlBuilder build() {
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append(sql).append(" ");
+      for (String condition : sqlConditions) {
+        stringBuilder.append(condition).append(" ");
+      }
+      this.sql = stringBuilder.toString();
+      return new SqlBuilder(this);
+    }
+  }
+
+  public SqlBuilder(Builder builder) {
+    this.sql = builder.sql;
+  }
+
+  public String getSql() {
+    return sql;
+  }
 }
