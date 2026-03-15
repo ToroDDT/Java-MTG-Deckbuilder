676ddb07217f1c52d68eb19c5871adea4c0e0271
David <Daviddeltoro1999@gmail.com>
2026-03-15 16:02:15 -0700

    Enhance `filterDecks` to include folder filtering and refactor method parameters with `@NonNull` annotation.

--- src/main/java/com/example/mtg_deckbuilder/utils/DeckUtils.java
+++ src/main/java/com/example/mtg_deckbuilder/utils/DeckUtils.java
@@ -1,24 +1,37 @@
 package com.example.mtg_deckbuilder.utils;
 
 import com.example.mtg_deckbuilder.model.Deck;
+import org.jspecify.annotations.NonNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class DeckUtils {
-    public static List<Deck> filterDecks(List<Deck> decks, DeckSearchCriteria criteria) {
-        String searchQuery = criteria.getSearchQuery();
-        List<String> selectedColors = criteria.getSelectedColors();
+    public static  List<Deck> filterDecks(List<Deck> decks, @NonNull DeckSearchCriteria criteria) {
+        var searchQuery = criteria.getSearchQuery();
+        var selectedColors = criteria.getSelectedColors();
+        var selectedFolder = criteria.getFolder();
 
         return decks.stream()
                 .filter(deck -> matchesSearchQuery(deck, searchQuery))
                 .filter(deck -> matchesSelectedColors(deck, selectedColors))
+                .filter(deck -> matchesFolder(deck, selectedFolder))
                 .toList();
     }
 
+    static public List<Deck> sortDecks(List<Deck> decks, @NonNull String sortBy) {
+        return switch (sortBy) {
+            case "commander" -> decks.stream()
+                    .sorted(Comparator.comparing(Deck::commander))
+                    .toList();
+            case "color identity" -> decks.stream()
+                    .sorted(Comparator.comparing(Deck::colors_identity))
+                    .toList();
+            default -> decks;
+        };
+    }
     private static boolean matchesSearchQuery(Deck deck, String searchQuery) {
         if (searchQuery == null || searchQuery.isEmpty()) {
             return true;
@@ -33,23 +46,13 @@ public class DeckUtils {
         return selectedColors.stream() .allMatch(color -> deck.colors_identity().contains(color));
     }
 
-    static public List<Deck> sortDecks(List<Deck> decks, String sortBy, String sortOrder) {
-        return switch (sortBy) {
-            case "commander" -> decks.stream()
-                    .sorted(Comparator.comparing(Deck::commander))
-                    .toList();
-            case "color identity" -> decks.stream()
-                    .sorted(Comparator.comparing(Deck::colors_identity))
-                    .toList();
-            default -> decks;
-        };
+    private static boolean matchesFolder(Deck deck, String folder) {
+        if (folder == null || folder.isEmpty() || folder.equals("home")) {
+            return true;
+        }
+        return deck.folder().contains(folder);
     }
 
-    static public List<String> getColorIdentityOfDeck(Deck deck) {
-        return Arrays.stream(deck.colors_identity().split(","))
-                .map(String::trim)
-                .toList();
-    }
     static public List<List<String>> getColorIdentityOfDecks(List<Deck> decks) {
          List<List<String>> colorIdentityList = new ArrayList<>();
          for (Deck deck : decks){
