6ad08fc6b0633331f6895e880678694314253303
David <Daviddeltoro1999@gmail.com>
2026-03-17 19:51:02 -0700

    Add Caffeine-based caching configuration

---
+++ src/main/java/com/example/mtg_deckbuilder/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package com.example.mtg_deckbuilder.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+
+@Configuration
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        var caffeine = Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.DAYS);
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
