0bf012c95955b1fed652566bac57536b936b7c9f
David <Daviddeltoro1999@gmail.com>
2026-03-21 21:28:34 -0700

    Remove debug e.printStackTrace() from AccountController

--- src/main/java/com/example/mtg_deckbuilder/controllers/AccountController.java
+++ src/main/java/com/example/mtg_deckbuilder/controllers/AccountController.java
@@ -39,7 +39,6 @@ public class AccountController {
             userService.saveUser(registrationDto);
         }
         catch (Exception e) {
-            e.printStackTrace();
             redirectAttributes.addFlashAttribute("error", e.getMessage());
             return "redirect:/register";
         }
