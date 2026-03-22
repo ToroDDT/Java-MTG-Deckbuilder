dafb4c23e6dfca6b963cba87a06011d0bbe92515
David <Daviddeltoro1999@gmail.com>
2026-03-22 15:37:10 -0700

    Add method to get color Identity

--- src/main/java/com/example/mtg_deckbuilder/repository/CardRowMapper.java
+++ src/main/java/com/example/mtg_deckbuilder/repository/CardRowMapper.java
@@ -2,13 +2,13 @@ package com.example.mtg_deckbuilder.repository;
 
 import com.example.mtg_deckbuilder.model.Card;
 import com.example.mtg_deckbuilder.model.ImageUris;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.jdbc.core.RowMapper;
 import tools.jackson.databind.ObjectMapper;
 
 import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.UUID;
 
 public class CardRowMapper implements RowMapper<Card> {
@@ -22,6 +22,7 @@ public class CardRowMapper implements RowMapper<Card> {
         card.setId(rs.getObject("id", UUID.class));
         card.setMultiverseIds(extractMultiverseIds(rs));
         card.setImage(extractArtCrop(rs));
+        card.setColorIdentity(List.of(extractColorIdentity(rs)));
         return card;
     }
 
@@ -29,12 +30,16 @@ public class CardRowMapper implements RowMapper<Card> {
         Array array = rs.getArray("multiverse_ids");
         return array != null ? (Integer[]) array.getArray() : new Integer[0];
     }
+    private String[] extractColorIdentity(ResultSet rs) throws SQLException {
+        Array array = rs.getArray("color_identity");
+        return array != null ? (String []) array.getArray() : new String[0];
+    }
 
     private String extractArtCrop(ResultSet rs) throws SQLException {
         String raw = rs.getString("image_uris");
         if (raw == null) return null;
         try {
-            return OBJECT_MAPPER.readValue(raw, ImageUris.class).getArtCrop();
+            return OBJECT_MAPPER.readValue(raw, ImageUris.class).getBorderCrop();
         } catch (Exception e) {
             throw new SQLException("Failed to deserialize image_uris", e);
         }
