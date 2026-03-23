f15c414c430fdf5648f9ba250e00709339ac8554
David <Daviddeltoro1999@gmail.com>
2026-03-22 21:52:42 -0700

    Refactor DeckCardEntry class to use lombok @getters and @setters

--- src/main/java/com/example/mtg_deckbuilder/model/DeckCardEntry.java
+++ src/main/java/com/example/mtg_deckbuilder/model/DeckCardEntry.java
@@ -1,11 +1,15 @@
 package com.example.mtg_deckbuilder.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 
+@Setter
+@Getter
 public class DeckCardEntry {
     @Column("id")
     private UUID id;
@@ -27,36 +31,4 @@ public class DeckCardEntry {
 
     @Column("updated_at")
     private LocalDateTime updatedAt;
-    public DeckCardEntry() {}
-
-    public DeckCardEntry(UUID id, UUID deckId, UUID cardId, boolean isSideboard, UUID personalCollectionLibraryId, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.deckId = deckId;
-        this.cardId = cardId;
-        this.isSideboard = isSideboard;
-        this.personalCollectionLibraryId = personalCollectionLibraryId;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public UUID getId() { return id; }
-    public void setId(UUID id) { this.id = id; }
-
-    public UUID getDeckId() { return deckId; }
-    public void setDeckId(UUID deckId) { this.deckId = deckId; }
-
-    public UUID getCardId() { return cardId; }
-    public void setCardId(UUID cardId) { this.cardId = cardId; }
-
-    public boolean isSideboard() { return isSideboard; }
-    public void setSideboard(boolean sideboard) { isSideboard = sideboard; }
-
-    public UUID getPersonalCollectionLibraryId() { return personalCollectionLibraryId; }
-    public void setPersonalCollectionLibraryId(UUID personalCollectionLibraryId) { this.personalCollectionLibraryId = personalCollectionLibraryId; }
-
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
-
-    public LocalDateTime getUpdatedAt() { return updatedAt; }
-    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
 }
