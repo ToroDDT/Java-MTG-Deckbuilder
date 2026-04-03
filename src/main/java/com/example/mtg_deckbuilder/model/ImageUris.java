c97b90597f566bafac7c941ad3dc1db56e6ad0df
David <Daviddeltoro1999@gmail.com>
2026-04-02 18:49:47 -0700

    Remove and added lombok @getters and @setters

--- src/main/java/com/example/mtg_deckbuilder/model/ImageUris.java
+++ src/main/java/com/example/mtg_deckbuilder/model/ImageUris.java
@@ -1,8 +1,11 @@
 package com.example.mtg_deckbuilder.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class ImageUris {
     private String small;
     private String normal;
@@ -14,52 +17,6 @@ public class ImageUris {
     private String artCrop;
 
     public ImageUris() {
-    }
-    public String getSmall() {
-        return small;
-    }
-
-    public void setSmall(String small) {
-        this.small = small;
-    }
-
-    public String getNormal() {
-        return normal;
-    }
-
-    public void setNormal(String normal) {
-        this.normal = normal;
-    }
-
-    public String getLarge() {
-        return large;
-    }
-
-    public void setLarge(String large) {
-        this.large = large;
-    }
-
-    public String getPng() {
-        return png;
-    }
-
-    public void setPng(String png) {
-        this.png = png;
-    }
-    public String getBorderCrop() {
-        return borderCrop;
-    }
-
-    public void setBorderCrop(String borderCrop) {
-        this.borderCrop = borderCrop;
-    }
-
-    public String getArtCrop() {
-        return artCrop;
-    }
-
-    public void setArtCrop(String artCrop) {
-        this.artCrop = artCrop;
     }
 
 
