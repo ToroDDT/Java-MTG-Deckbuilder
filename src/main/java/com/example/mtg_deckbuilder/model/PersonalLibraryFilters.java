7a3d63d900622028e97c8f1db605b10030e12854
David <Daviddeltoro1999@gmail.com>
2026-04-05 18:31:06 -0700

    Add `PersonalLibraryFilters` model to represent filter options for the personal library

---
+++ src/main/java/com/example/mtg_deckbuilder/model/PersonalLibraryFilters.java
@@ -0,0 +1,20 @@
+package com.example.mtg_deckbuilder.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class PersonalLibraryFilters {
+    private String cardName;
+    private List<String> selectedColors = new ArrayList<>();
+    private String cardType;
+    private SortOptions sortBy;
+
+    public PersonalLibraryFilters() {
+
+    }
+}
