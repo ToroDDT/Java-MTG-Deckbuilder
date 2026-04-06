ca5588d83ce4c3468f1cc4830f687d1abcc7579f
David <Daviddeltoro1999@gmail.com>
2026-04-05 22:16:17 -0700

    Add min and max cmc filters

--- src/main/java/com/example/mtg_deckbuilder/model/PersonalLibraryFilters.java
+++ src/main/java/com/example/mtg_deckbuilder/model/PersonalLibraryFilters.java
@@ -12,6 +12,8 @@ public class PersonalLibraryFilters {
     private String cardName;
     private List<String> selectedColors = new ArrayList<>();
     private String cardType;
+    private Integer minCMC;
+    private Integer maxCMC;
     private SortOptions sortBy;
 
     public PersonalLibraryFilters() {
