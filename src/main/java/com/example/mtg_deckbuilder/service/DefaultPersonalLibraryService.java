a93e18aba648cc24ea43766d5b1b112d81839ff6
David <Daviddeltoro1999@gmail.com>
2026-04-06 20:01:38 -0700

    Add sorting fitlers for cards

--- src/main/java/com/example/mtg_deckbuilder/service/DefaultPersonalLibraryService.java
+++ src/main/java/com/example/mtg_deckbuilder/service/DefaultPersonalLibraryService.java
@@ -1,15 +1,15 @@
 package com.example.mtg_deckbuilder.service;
 
 import com.example.mtg_deckbuilder.exceptions.CardDoesNotExistException;
-import com.example.mtg_deckbuilder.model.CardType;
-import com.example.mtg_deckbuilder.model.OwnedCard;
-import com.example.mtg_deckbuilder.model.PersonalLibraryFilters;
+import com.example.mtg_deckbuilder.model.*;
 import com.example.mtg_deckbuilder.repository.DefaultPersonalLibraryRepository;
 import com.example.mtg_deckbuilder.repository.PersonalLibraryRepository;
 import com.example.mtg_deckbuilder.utils.CardUtils;
 import org.springframework.stereotype.Service;
 
+import javax.swing.*;
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
@@ -47,12 +47,34 @@ public class DefaultPersonalLibraryService implements PersonalLibraryService {
     public List<OwnedCard> getCardsFromPersonalLibrary(UUID userid, PersonalLibraryFilters personalLibraryFilters) {
 
         var cardType = CardType.fromString(personalLibraryFilters.getCardType());
+        SortOptions sortBy = personalLibraryFilters.getSortBy();
 
         return personalLibraryRepository.getAllPersonalLibraryCardsForUser(userid).stream()
                 .filter(card -> CardUtils.matchesSearchQuery(card, personalLibraryFilters.getCardName()))
                 .filter(card -> CardUtils.matchesSelectedColors(card, personalLibraryFilters.getSelectedColors()))
                 .filter(card -> CardUtils.matchesSelectedType(card, cardType))
                 .filter(card -> CardUtils.matchesCmcRange(card, personalLibraryFilters))
+                .sorted(switch (sortBy) {
+                    case PRICE_ASC -> Comparator.comparing(
+                            (OwnedCard ownedCard) -> ownedCard.getCard().getPrices().getUsd(),
+                            Comparator.nullsLast(Comparator.naturalOrder())
+                    );
+
+                    case PRICE_DESC -> Comparator.comparing(
+                            (OwnedCard ownedCard) -> ownedCard.getCard().getPrices().getUsd(),
+                            Comparator.nullsLast(Comparator.reverseOrder())
+                    );                   case CMC_ASC-> Comparator.comparing(ownedCard ->
+                            ownedCard
+                                    .getCard()
+                                    .getCmc());
+                    case CMC_DESC-> Comparator.comparing((OwnedCard ownedCard) ->
+                                    ownedCard
+                                            .getCard()
+                                            .getCmc())
+                            .reversed();
+                    case NAME_DESC-> Comparator.comparing(OwnedCard::getName).reversed();
+                    default -> Comparator.comparing(OwnedCard::getName); // or whatever your default sort is
+                })
                 .toList();
     }
 }
