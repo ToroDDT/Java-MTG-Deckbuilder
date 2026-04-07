e9594dc0f2e08950c06d90591a5602ed5e722e35
David <Daviddeltoro1999@gmail.com>
2026-04-06 19:56:23 -0700

    Rename PersonalLibraryService with additional parameters to include filters

--- src/main/java/com/example/mtg_deckbuilder/service/PersonalLibraryService.java
+++ src/main/java/com/example/mtg_deckbuilder/service/PersonalLibraryService.java
@@ -1,6 +1,7 @@
 package com.example.mtg_deckbuilder.service;
 
 import com.example.mtg_deckbuilder.model.OwnedCard;
+import com.example.mtg_deckbuilder.model.PersonalLibraryFilters;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,4 +9,5 @@ import java.util.UUID;
 public interface PersonalLibraryService {
     void addCardToPersonalLibrary(OwnedCard ownedCard, UUID userId);
     List<OwnedCard> getCardsFromPersonalLibrary(UUID userId);
+    List<OwnedCard> getCardsFromPersonalLibrary(UUID userId, PersonalLibraryFilters personalLibraryFilters);
 }
