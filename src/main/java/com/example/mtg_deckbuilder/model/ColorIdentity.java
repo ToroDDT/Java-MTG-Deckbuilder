23b1a693116b9dd8b4308b5471a2dc45ce0de49c
David <Daviddeltoro1999@gmail.com>
2026-04-06 20:01:05 -0700

    Add filtering and sorting support for personal library

--- src/main/java/com/example/mtg_deckbuilder/model/ColorIdentity.java
+++ src/main/java/com/example/mtg_deckbuilder/model/ColorIdentity.java
@@ -1,36 +1,20 @@
 package com.example.mtg_deckbuilder.model;
 
-import java.util.Arrays;
-import java.util.List;
 
+import lombok.Getter;
+
+@Getter
 public enum ColorIdentity {
-   // Single Colors
-    WHITE("{W}", "White", "W"),
-    BLUE("{U}", "Blue", "U"),
-    BLACK("{B}", "Black", "B"),
-    RED("{R}", "Red", "R"),
-    GREEN("{G}", "Green", "G"),
-    COLORLESS("{}", "Colorless"),
+    WHITE("W"),
+    BLUE("U"),
+    BLACK("B"),
+    RED("R"),
+    GREEN("G"),
+    COLORLESS("C"),
+ ;
+    private final String colorIdentity;
 
-    // Guilds (2-Color Pairs)
-    AZORIUS("{W}{U}", "Azorius", "W", "U"),
-    DIMIR("{U}{B}", "Dimir", "U", "B"),
-    RAKDOS("{B}{R}", "Rakdos", "B", "R"),
-    GRUUL("{R}{G}", "Gruul", "R", "G"),
-    SELESNYA("{G}{W}", "Selesnya", "G", "W"),
-    ORZHOV("{W}{B}", "Orzhov", "W", "B"),
-    IZZET("{U}{R}", "Izzet", "U", "R"),
-    GOLGARI("{B}{G}", "Golgari", "B", "G"),
-    BOROS("{R}{W}", "Boros", "R", "W"),
-    SIMIC("{G}{U}", "Simic", "G", "U");
-
-    private final String nameBrackets;
-    private final String colorIdentityName;
-    private final List<String>colorIdentityAsList;
-
-    ColorIdentity(String nameBrackets, String colorIdentityName,String... colorIdentityAsList) {
-        this.nameBrackets = nameBrackets;
-        this.colorIdentityName = colorIdentityName;
-        this.colorIdentityAsList = Arrays.asList(colorIdentityAsList) ;
-    };
+ ColorIdentity(String s) {
+     this.colorIdentity = s;
+ }
 }
