3df09c329bc53a2d8c9190e6bb34c47d5b09a765
David <Daviddeltoro1999@gmail.com>
2026-04-10 17:46:45 -0700

    Refactor to builder

--- src/main/java/com/example/mtg_deckbuilder/model/CardPrices.java
+++ src/main/java/com/example/mtg_deckbuilder/model/CardPrices.java
@@ -1,11 +1,13 @@
 package com.example.mtg_deckbuilder.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class CardPrices {
     @JsonProperty("usd")
     private Double usd;
@@ -24,15 +26,4 @@ public class CardPrices {
 
     @JsonProperty("tix")
     private Double tix;
-    public CardPrices() {}
-
-    public CardPrices(Double usd, Double usdFoil, Double usdEtched,
-                     Double eur, Double eurFoil, Double tix) {
-        this.usd = usd;
-        this.usdFoil = usdFoil;
-        this.usdEtched = usdEtched;
-        this.eur = eur;
-        this.eurFoil = eurFoil;
-        this.tix = tix;
-    }
 }
