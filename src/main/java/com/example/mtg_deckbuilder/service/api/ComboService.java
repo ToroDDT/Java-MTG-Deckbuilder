36dd97bbc802ace9e5b21bac213700dae01b2b72
David <Daviddeltoro1999@gmail.com>
2026-04-15 15:40:55 -0700

    Add to parameter list

--- src/main/java/com/example/mtg_deckbuilder/service/api/ComboService.java
+++ src/main/java/com/example/mtg_deckbuilder/service/api/ComboService.java
@@ -1,8 +1,9 @@
 package com.example.mtg_deckbuilder.service.api;
 
 import com.example.mtg_deckbuilder.dto.CardCombos;
+import com.example.mtg_deckbuilder.model.LibraryFilters;
 import com.example.mtg_deckbuilder.security.CustomUserDetails;
 
 public interface ComboService {
-    CardCombos findCombos(CustomUserDetails userId) throws Exception;
+    CardCombos findCombos(CustomUserDetails userId, LibraryFilters libraryFilters) throws Exception;
 }
