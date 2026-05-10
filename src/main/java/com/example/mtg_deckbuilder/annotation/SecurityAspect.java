1248ebc108ecb97e090c65c757cb74008497f827
David <Daviddeltoro1999@gmail.com>
2026-05-10 15:33:56 -0700

    Add redirects if the user is not loged in

--- src/main/java/com/example/mtg_deckbuilder/annotation/SecurityAspect.java
+++ src/main/java/com/example/mtg_deckbuilder/annotation/SecurityAspect.java
@@ -4,10 +4,10 @@ import com.example.mtg_deckbuilder.security.CustomUserDetails;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.stereotype.Component;
 
 import javax.naming.AuthenticationException;
+
 
 @Aspect
 @Component
@@ -15,18 +15,16 @@ public class SecurityAspect {
 
     @Around("@annotation(com.example.mtg_deckbuilder.annotation.RequiresAuth)")
     public Object checkUser(ProceedingJoinPoint joinPoint) throws Throwable {
-        // Look through the method arguments for the CustomUserDetails
         Object[] args = joinPoint.getArgs();
 
         for (Object arg : args) {
             if (arg instanceof CustomUserDetails user) {
                 if (user.getId() == null) {
-                    throw new InsufficientAuthenticationException("You must be logged in to do this.");
+                    throw new AuthenticationException("You must be logged in to do this.");
                 }
-                return joinPoint.proceed(); // User is fine, continue to the method
+                return joinPoint.proceed();
             }
         }
-
-        throw new InsufficientAuthenticationException("Security context missing.");
+        throw new AuthenticationException("Security context missing.");
     }
 }
